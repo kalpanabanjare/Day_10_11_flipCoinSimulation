63d2d04635c5c3a1e35dfb53a3d13b1aa4a4c080
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-02-24 14:27:38 +0530

    Added: who win & by what times

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -2,18 +2,34 @@
 
 head_count=0
 tail_count=0
-
-while(( $head_count != 10 && $tail_count != 10 ))
+while(( $head_count != 21 && $tail_count != 21 ))
 do
-if (( $RANDOM%2 == 1 ))
+if (( $RANDOM%3 == 1 ))
 then
      echo "Head"
      (( head_count++ ))
-else
+elif(( $RANDOM%3 == 0 ))
+then
      echo "Tail"
      (( tail_count++ ))
+else
+     echo "Tie"
 fi
 done
 
 echo "Head count:" $head_count
 echo "Tail count:" $tail_count
+echo "Tie count:" $tie_count
+
+if(( $head_count > $tail_count ))
+then
+    echo "Head wins"
+elif(( $head_count < $tail_count ))
+then
+    echo "Tail wins"
+else
+    echo "Tie"
+fi
+
+won_by=$(( $head_count - $tail_count ))
+echo "won by:" $won_by
