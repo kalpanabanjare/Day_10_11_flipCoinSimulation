49c19e3925490927dd80b0e3a7cbbce9851daffc
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-02-24 15:30:11 +0530

    Added: again flip the coin tillhead or tail win by 2

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -2,7 +2,9 @@
 
 head_count=0
 tail_count=0
-while(( $head_count != 21 && $tail_count != 21 ))
+tie_count=0
+
+while(( $head_count != 21 && $tail_count != 21 && $tie_count != 21 ))
 do
 if (( $RANDOM%3 == 1 ))
 then
@@ -14,21 +16,55 @@ then
      (( tail_count++ ))
 else
      echo "Tie"
+     (( tie_count++ ))
 fi
 done
 
 echo "Head count:" $head_count
 echo "Tail count:" $tail_count
+echo "Tie count:" $tie_count
 
-if(( $head_count > $tail_count ))
+if(( $head_count > $tail_count && $head_count > $tie_count ))
 then
     echo "Head wins"
-elif(( $head_count < $tail_count ))
+    won_by=$(( $head_count - $tail_count ))
+    echo "Head won by:" $won_by
+elif(( $tail_count > $head_count && $tail_count > $tie_count ))
 then
     echo "Tail wins"
+    won_by=$(( $tail_count - $head_count ))
+    echo "Tail won by:" $won_by
 else
     echo "Tie"
 fi
 
-won_by=$(( $head_count - $tail_count ))
-echo "won by:" $won_by
+head_count_1=0
+tail_count_1=0
+if(( $tie_count == 21 ))
+then
+     echo "Flip the coin again till head or tail win by 2 times"
+     while(( $head_count_1 != 2 && $tail_count_1 != 2 ))
+     do
+         if(( $RANDOM%2 == 1 ))
+         then
+              (( head_count_1++ ))
+         else
+              (( tail_count_1++ ))
+         fi
+     done
+
+echo "Head count:" $head_count_1
+echo "Tail count:" $tail_count_1
+fi
+
+if(( $head_count_1 > $tail_count_1 ))
+then
+    echo "Head Wins"
+    echo "Head win by:" $head_count_1
+elif(( $tail_count_1 > $head_count_1 ))
+then
+    echo "Tail Wins"
+    echo "Tail win by:" $tail_count_1
+else
+    exit
+fi
